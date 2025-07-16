cd31017c37aac2b9fc3d5e8360adad5fdf871744
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-07-16 08:44:52 -0300

    feat: add NavBar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+
 
 export default function Home() {
   return (
    <main className="flex min-h-screen flex-col bg-black">
-    <div class="container mx-auto px-12 py-4">
+    <Navbar />
+    <div className="container mt-24 mx-auto px-12 py-4">
       <HeroSection />
     </div>
   </main>
